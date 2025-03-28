c53b9fd2ce22b8eb83ae7212d5bf887f7c7862ab
Jean-GabrielFrenoyUpssitech <jean-gabriel.frenoy@univ-tlse3.fr>
2025-03-28 12:28:59 +0100

    Modif serveur et client le try ancienne backup qui fonctionne plus ou moins

--- src/jeu/Client.java
+++ src/jeu/Client.java
@@ -9,7 +9,7 @@ public class Client {
         String SERVER_IP = "lespiratesjeu.up.railway.app"; // Remplace par ton URL Railway
         int SERVER_PORT = 5000; // Assure-toi que c'est bien le port utilisé sur Railway
 
-        try (Socket socket = new Socket("lespiratesjeu.up.railway.app", 5000);
+        try (Socket socket = new Socket("lespiratesjeu.up.railway.app", 12345);
              BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
              Scanner scanner = new Scanner(System.in)) {
